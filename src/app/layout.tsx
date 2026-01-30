90544c042a8fc22ead84f5c0d4b5a716af048b60
David-Alexandre Guenette <daguenette.data@gmail.com>
2026-01-30 08:33:28 -0500

    Fix browser tab title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,51 +36,12 @@ const geistMono = Geist_Mono({
   preload: false,
 });
 
-// TODO: Update all metadata with TechVapor information
 export const metadata: Metadata = {
-  metadataBase: new URL("https://techvapor.com"), // TODO: Update with actual domain
-  title: "TechVapor - [TODO: Add your site title]",
-  description:
-    "[TODO: Add your company description for SEO - explain what TechVapor does and the value you provide]",
-  keywords: "TechVapor, [TODO: Add relevant keywords for your business]",
-  authors: [{ name: "TechVapor" }],
-  icons: {
-    icon: [
-      { url: "/vh_logo.svg", type: "image/svg+xml" }, // TODO: Replace with your logo
-      { url: "/vh_logo.svg", sizes: "any" }, // TODO: Replace with your logo
-    ],
-    shortcut: "/vh_logo.svg", // TODO: Replace with your logo
-    apple: "/vh_logo.svg", // TODO: Replace with your logo
-  },
-  openGraph: {
-    title: "TechVapor - [TODO: Add Open Graph title]",
-    description: "[TODO: Add Open Graph description for social media sharing]",
-    type: "website",
-    locale: "en_CA", // TODO: Update if different locale
-    siteName: "TechVapor",
-    images: [
-      {
-        url: "/vh_logo.svg", // TODO: Replace with your logo
-        width: 1024,
-        height: 1024,
-        alt: "TechVapor - [TODO: Add logo alt text]",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "TechVapor - [TODO: Add Twitter card title]",
-    description: "[TODO: Add Twitter card description]",
-    images: ["/vh_logo.svg"], // TODO: Replace with your logo
-  },
+  // Temporary: simple, correct browser tab title. (We can refine SEO metadata later.)
+  title: "Nettoyeur Technivapeur",
   robots: {
     index: true,
     follow: true,
-  },
-  other: {
-    // Resource hints for performance
-    "dns-prefetch": "//fonts.googleapis.com //fonts.gstatic.com",
-    preconnect: "https://fonts.googleapis.com https://fonts.gstatic.com",
   },
 };
 
@@ -104,9 +65,7 @@ export default function RootLayout({
           content="black-translucent"
         />
         <style dangerouslySetInnerHTML={{ __html: criticalCSS }} />
-        <link rel="icon" href="/vh_logo.svg" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/vh_logo.svg" />
-        <link rel="apple-touch-icon" href="/vh_logo.svg" />
+        {/* Intentionally no custom favicon for now */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
